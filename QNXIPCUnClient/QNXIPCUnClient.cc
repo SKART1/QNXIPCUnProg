d6ba8584f9fcf381a9561e6ff646b655fc445093
SKART1 <art.enikoff@gmail.com>
2014-05-10 15:24:17 +0400

    Corrected little bug

--- QNXIPCUnClient/QNXIPCUnClient.cc
+++ QNXIPCUnClient/QNXIPCUnClient.cc
@@ -8,15 +8,47 @@
 #include "QNXIPCUnClient.hpp"
 
 
+
+int readFromFile(AboutServerInfoStruct *aboutServerInfoStructl){
+	FILE *filePointer = NULL;
+	filePointer = fopen((aboutServerInfoStructl->pathToFileWithServerInfo).c_str(), "r");
+	if (filePointer == NULL) {
+		printf("[ERROR]: %d can not open file with program information because of: %s\n",	errno, strerror(errno));
+		return -1;
+	}
+
+	char buff[100];
+	fscanf(filePointer, "%s", buff);
+	fscanf(filePointer, "%d", &((*aboutServerInfoStructl).pid));
+
+	fscanf(filePointer, "%s", buff);
+	fscanf(filePointer, "%d", &((*aboutServerInfoStructl).ppid));
+
+	fscanf(filePointer, "%s", buff);
+	fscanf(filePointer, "%d", &((*aboutServerInfoStructl).tid));
+
+	fscanf(filePointer, "%s", buff);
+	fscanf(filePointer, "%d",&((*aboutServerInfoStructl).fileDes[0]));
+
+	fscanf(filePointer, "%s", buff);
+	fscanf(filePointer, "%d",&((*aboutServerInfoStructl).fileDes[1]));
+
+	return 0;
+}
+
+
+
+
 int main(int argc, char *argv[]) {
 	std::cout << "Welcome to the client!" << std::endl;
 
-	/*BEGIN: !!!!!!!!!!!!!!!Processing input keys!!!!!!!!!!!!!!!*/
-	IPCType IPCTypeSelector = signalIPC;
-	participantsType participantsTypeSelector = oneProcessThreads;
-
-	parseParametrsMy(argc, argv, &IPCTypeSelector, &participantsTypeSelector);
+	AboutServerInfoStruct aboutServerInfoStruct;
+	parseParametrsMy(argc, argv, &aboutServerInfoStruct);
 	/*END: !!!!!!!!!!!!!!!Processing input keys!!!!!!!!!!!!!!!*/
+
+
+	readFromFile(&aboutServerInfoStruct);
+	//aboutServerInfoStructl.IPCTypeSelector=IPCTypeSelector;
 
 
 	sleep(2);
